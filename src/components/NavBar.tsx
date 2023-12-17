dc70882c54ffbb3a677c2d96842a728f465bea67
radouane <oubakhaneradouane@gmail.com>
2023-12-17 20:48:16 +0100

    Highlight selected section in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -17,10 +17,12 @@ import AuthorSelector from "./AuthorSelector";
 import CategorySelector from "./CategorySelector";
 import ColorModeSwitch from "./ColorModeSwitch";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const NavBar = () => {
   const { data: categories, error: categoryError } = useCategories();
   const { data: authors, error: authorError } = useAuthors();
+  const [selectedSection, setSelectedSection] = useState("home");
 
   if (categoryError && authorError) {
     return null;
@@ -28,49 +30,59 @@ const NavBar = () => {
 
   return (
     <>
-    <HStack justifyContent="space-between" padding="20px">
-      <HStack justifyContent="space-between" spacing={10}>
-        <Link to="/">
-          <Text>Logo</Text>
-        </Link>
-        <CategorySelector categories={categories} />
-        <AuthorSelector authors={authors} />
-        <Link to="/inscriptions">
-          <Heading as="b" size="sm" whiteSpace="nowrap">Inscriptions</Heading>
-        </Link>
-        <Link to="/categories">
-          <Heading as="b" size="sm" whiteSpace="nowrap">Categories</Heading>
-        </Link>
-        <Link to="/books">
-          <Heading as="b" size="sm" whiteSpace="nowrap">Books</Heading>
-        </Link>
-        <Link to="/authors">
-          <Heading as="b" size="sm" whiteSpace="nowrap">Authors</Heading>
-        </Link>
-      </HStack>
-      <Spacer />
-
-      <HStack justifyContent="space-between" spacing={4}>
-        <ColorModeSwitch />
-        <Menu>
-          <MenuButton>
-            <Heading as="b" size="sm" whiteSpace="nowrap">
-              <Button colorScheme="gray">Radouane</Button>
+      <HStack justifyContent="space-between" padding="20px">
+        <HStack justifyContent="space-between" spacing={10}>
+          <Link to="/">
+            <Text>Logo</Text>
+          </Link>
+          <CategorySelector categories={categories} />
+          <AuthorSelector authors={authors} />
+          <Link to="/inscriptions">
+            <Heading as="b" size="sm" whiteSpace="nowrap" onClick={() => setSelectedSection("inscriptions")} textColor={selectedSection === "inscriptions" ? "blue.500" : ""}>
+              Inscriptions
             </Heading>
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Profile</MenuItem>
-            <MenuItem>Sign Up</MenuItem>
-            <MenuItem>Sign In</MenuItem>
-            <MenuItem>Sign Out</MenuItem>
-          </MenuList>
-        </Menu>
-        <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+          </Link>
+          <Link to="/categories">
+            <Heading as="b" size="sm" whiteSpace="nowrap" onClick={() => setSelectedSection("categories")} textColor={selectedSection === "categories" ? "blue.500" : ""}>
+              Categories
+            </Heading>
+          </Link>
+          <Link to="/books">
+            <Heading as="b" size="sm" whiteSpace="nowrap" onClick={() => setSelectedSection("books")} textColor={selectedSection === "books" ? "blue.500" : ""}>
+              Books
+            </Heading>
+          </Link>
+          <Link to="/authors">
+            <Heading as="b" size="sm" whiteSpace="nowrap" onClick={() => setSelectedSection("authors")} textColor={selectedSection === "authors" ? "blue.500" : ""}>
+              Authors
+            </Heading>
+          </Link>
+        </HStack>
+        <Spacer />
+
+        <HStack justifyContent="space-between" spacing={4}>
+          <ColorModeSwitch />
+          <Menu>
+            <MenuButton>
+              <Heading as="b" size="sm" whiteSpace="nowrap">
+                <Button colorScheme="gray">Radouane</Button>
+              </Heading>
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Profile</MenuItem>
+              <MenuItem>Sign Up</MenuItem>
+              <MenuItem>Sign In</MenuItem>
+              <MenuItem>Sign Out</MenuItem>
+            </MenuList>
+          </Menu>
+          <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+        </HStack>
       </HStack>
-    </HStack>
-    <Divider orientation="horizontal"/>
+      <Divider orientation="horizontal" />
     </>
   );
 };
 
 export default NavBar;
+
+
